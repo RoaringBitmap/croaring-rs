a185b206bf91d744d19ff9bf0faab2499b1a593f
Saulius Grigaliunas <saulius@ninja.lt>
2017-05-03 16:28:30 +0300

    Use Rust's bindgen to generate FFI bindings
    No need to create these definitions manually.

--- croaring-sys/build.rs
+++ croaring-sys/build.rs
@@ -1,4 +1,8 @@
+extern crate bindgen;
 extern crate gcc;
+
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
     let mut config = gcc::Config::new();
@@ -8,4 +12,16 @@ fn main() {
     config.flag("-O3");
     config.file("CRoaring/roaring.c");
     config.compile("libroaring.a");
+
+    let bindings = bindgen::Builder::default()
+        .no_unstable_rust()
+        .header("CRoaring/roaring.h")
+        .generate_inline_functions(true)
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("croaring-sys.rs"))
+        .expect("Couldn't write bindings!");
 }
