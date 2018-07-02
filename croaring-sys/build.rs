49ad991839ff21b75b241d47e027b08fa8054fe6
Saulius Grigaliūnas <saulius@ninja.lt>
2018-07-02 21:33:30 +0300

    Blacklist max_align_t bindgen type
    As suggested in https://github.com/rust-lang-nursery/rust-bindgen/issues/550

--- croaring-sys/build.rs
+++ croaring-sys/build.rs
@@ -13,6 +13,7 @@ fn main() {
       .compile("libroaring.a");
 
     let bindings = bindgen::Builder::default()
+        .blacklist_type("max_align_t")
         .header("CRoaring/roaring.h")
         .generate_inline_functions(true)
         .generate()
