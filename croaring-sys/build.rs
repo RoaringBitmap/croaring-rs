dbf54c0a84221af6704c9c76ad4511c30bb7299b
Saulius Grigaliūnas <saulius@ninja.lt>
2019-01-11 10:37:18 +0200

    Build CRoaring with flags if compiler supports them
    Prevents warning during Windows build. Possibly fixes #36.

--- croaring-sys/build.rs
+++ croaring-sys/build.rs
@@ -6,9 +6,9 @@ use std::path::PathBuf;
 
 fn main() {
     cc::Build::new()
-        .flag("-std=c11")
-        .flag("-march=native")
-        .flag("-O3")
+        .flag_if_supported("-std=c11")
+        .flag_if_supported("-march=native")
+        .flag_if_supported("-O3")
         .file("CRoaring/roaring.c")
         .compile("libroaring.a");
 
