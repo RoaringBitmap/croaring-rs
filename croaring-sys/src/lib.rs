48bb8dd87fe044eb58619bdaf4660b0eee21a01e
Zachary Dremann <dremann@gmail.com>
2022-03-21 21:20:49 -0400

    Improvements to croaring-sys
    * Use allowlists to only include croaring items
    * Include docs
    * Output extra cargo metadata to tell it about the environment variable
      our build script uses

--- croaring-sys/src/lib.rs
+++ croaring-sys/src/lib.rs
@@ -1,5 +1,6 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(rustdoc::bare_urls)]
 
 include!(concat!(env!("OUT_DIR"), "/croaring-sys.rs"));
