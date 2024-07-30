75f83a23fdb4c8822f9187f858ad55614ce01742
Zachary Dremann <dremann@gmail.com>
2024-07-30 15:28:31 -0400

    Bump to croaring 4.1.1
    No API changes, just implementation

--- croaring-sys/build.rs
+++ croaring-sys/build.rs
@@ -8,7 +8,7 @@ fn main() {
     build.file("CRoaring/roaring.c");
 
     if let Ok(target_arch) = env::var("ROARING_ARCH") {
-        build.flag_if_supported(&format!("-march={target_arch}"));
+        build.flag_if_supported(format!("-march={target_arch}"));
     }
 
     build.flag_if_supported("-Wno-unused-function");
