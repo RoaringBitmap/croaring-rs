5adb10dea7a24209460fabcd363e0290977fe48b
Zachary Dremann <dremann@gmail.com>
2025-10-01 23:18:43 -0400

    chore: fix docs.rs builds
    doc_auto_cfg has been merged into doc_cfg in rust-lang/rust#138907

--- croaring/src/lib.rs
+++ croaring/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![deny(missing_docs)]
 //! Rust wrapper for `CRoaring` (a C/C++ implementation at <https://github.com/RoaringBitmap/CRoaring>)
 //!
